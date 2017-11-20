dd419f8657c0cc1b0ec72d191aae1f2b23e59065
RevanRobots <32439228+RevanRobots@users.noreply.github.com>
2017-11-19 18:45:22 -0600

    Add files via upload

---
+++ Event_1.ino
@@ -0,0 +1,26 @@
+const int in1 = 2;
+const int in2 = 3;
+const int enA = 5;
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enA, OUTPUT);
+  Serial.begin(4800);
+
+}
+
+void loop() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, 255);
+  Serial.println("On");
+
+  delay(5000);
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  Serial.println("off");
+
+  delay(5000);
+
+}
